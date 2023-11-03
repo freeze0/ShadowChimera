26eed3b05a1abe6f21ee20334aa13b405c66ff0b
freeze0 <kras2001@bk.ru>
2023-11-03 16:56:14 +0300

    test

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,16 +14,19 @@ namespace ShadowChimera
 
         private InputActionMap m_playerMap;
         private InputAction m_moveAction;
+        private InputAction m_jumpAction;
 
         private void Awake()
         {
             m_playerMap = m_inputActionAsset.FindActionMap("Player");
             m_moveAction = m_playerMap.FindAction("Move");
+            m_jumpAction = m_playerMap.FindAction("Jump");
         }
 
         private void OnEnable()
         {
             m_playerMap.Enable();
+           // m_jumpAction.performed += Jump();
         }
 
         private void OnDisable()
@@ -33,13 +37,17 @@ namespace ShadowChimera
         private void Update()
         {
             Vector2 move = m_moveAction.ReadValue<Vector2>();
-
-            if (move != Vector2.zero) 
+            
+            if (move != Vector2.zero)
             {
                 Vector3 dir = new Vector3(move.x, 0f, move.y);
-                //Debug.Log($"{move}"); 
                 m_characterController.SimpleMove(dir * m_speedMove);
             }
+        }
+
+        private void Jump()
+        {
+            
         }
     }
 }
