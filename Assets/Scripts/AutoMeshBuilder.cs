b95a18f1800c32c422bce82e67a28babfbeaf277
freeze0 <kras2001@bk.ru>
2023-11-09 16:36:17 +0300

    Finish PlayerController + Started navMesh

--- Assets/Scripts/AutoMeshBuilder.cs
+++ Assets/Scripts/AutoMeshBuilder.cs
@@ -1,21 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEngine;
 
 namespace ShadowChimera
 {
     public class AutoMeshBuilder : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        private void Awake()
         {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
+            var surfaces = GetComponents<NavMeshSurface>();
+            foreach (var surface in surfaces)
+            {
+                surface.BuildNavMesh();
+            }
         }
     }
 }
